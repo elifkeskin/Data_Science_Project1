8903fb3c13f2f9efa2cf63117b19a13187eb2a14
Elif Keskin <90678194+elifkeskin@users.noreply.github.com>
2025-06-10 20:16:20 +0300

    First code (#2)
    * Move ReadME, toml and lock to root folder
    
    Add data loading functionality with error handling and logging
    
    Add test suite for DataLoader with file extension validation
    
    Rename Project from claims_ml to frigg_ml
    
    Add test fixtures and datasets for DataLoader tests
    
    Updated dependecies
    
    * Move ReadME, toml and lock to root folder
    
    Add data loading functionality with error handling and logging
    
    Add test suite for DataLoader with file extension validation
    
    Rename Project from claims_ml to frigg_ml
    
    Add test fixtures and datasets for DataLoader tests
    
    Updated dependecies

---
+++ frigg_ml/src/data_loader/__init__.py
@@ -0,0 +1,2 @@
+from .data_loader import DataLoader
+from .error_messages import DataReadingErrorMessages as EM, SUPPORTED_FILE_EXTENSIONS
---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+# manuel olarak claims_ml'in nerede olduğunu belirtmek için
+
+import sys
+import os
+
+import pytest
+import pandas as pd
+from pathlib import Path
+
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+
+@pytest.fixture
+def test_datasets_path():
+    """Fixture to provide the path to the test datasets."""
+    return Path(project_root) / "tests" / "test_datasets"
+
+
+@pytest.fixture
+def empty_dataset(test_datasets_path):
+    """Fixture to provide an empty dataset path."""
+    return pd.read_csv(test_datasets_path / "test_empty.csv")
+
+
+@pytest.fixture
+def _csv_data(test_datasets_path):
+    """Fixture to load CSV data for testing."""
+    return pd.read_csv(test_datasets_path / "test.csv")
+
+
+@pytest.fixture
+def _parquet_data(test_datasets_path):
+    """Fixture to load Parquet data for testing."""
+    return pd.read_parquet(test_datasets_path / "test.parquet")
